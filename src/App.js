081e6686fd3f6d7d7fd483971463a322676d968e
VazquezAlex <alejandrovazquez.dev@gmail.com>
2024-06-10 16:49:36 -0600

    added env variable

--- src/App.js
+++ src/App.js
@@ -2,9 +2,13 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  const message = process.env.REACT_APP_TEST;
+
   return (
     <div className="App">
       <header className="App-header">
+        <p>{ message }</p>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
